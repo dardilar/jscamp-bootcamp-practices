74bdad6197dd4cebdd8dcf95a21eeb10343c5202
dsardila <dsardila1809@hotmail.com>
2026-02-04 19:34:53 -0500

    [ADD] Segunda parte | Renderizados condicionales y mapeo de datos

--- 03-ejercicio-react/src/components/SearchResultsSection.jsx
+++ 03-ejercicio-react/src/components/SearchResultsSection.jsx
@@ -1,27 +1,37 @@
+import JobsData from "../data.json";
+
 export function SearchResultsSection() {
   return (
     <section>
       <h2 style={{ textAlign: "center" }}>Resultados de búsqueda</h2>
 
       <div className="jobs-listings">
-        <article
-          className="job-listing-card"
-          data-modalidad="remoto"
-          data-nivel="senior"
-          data-technology="javascript"
-        >
-          <div>
-            <h3>Desarrollador de Software Senior</h3>
-            <small>Tech Solutions Inc. | Remoto</small>
-            <p>
-              Buscamos un ingeniero de software con experiencia en desarrollo
-              web y conocimientos en JavaScript, React y Node.js. El candidato
-              ideal debe ser capaz de trabajar en equipo y tener buenas
-              habilidades de comunicación.
-            </p>
-          </div>
-          <button className="button-apply-job">Aplicar</button>
-        </article>
+        {
+            JobsData.length === 0 && (
+                <p style={{ textAlign: "center", padding: "2rem" }}>No se han encontrado empleos que coincidan con los criterios de búsqueda</p>
+            )
+        }
+
+        {JobsData.map((job) => {
+          return (
+            <article
+              className="job-listing-card"
+              data-modalidad={job.data.modalidad}
+              data-nivel={job.data.nivel}
+              data-technology={job.data.technology}
+            >
+              <div>
+                <h3>{job.titulo}</h3>
+                <small>{job.empresa} | {job.ubicacion}</small>
+                <p>
+                  {job.descripcion}
+                </p>
+              </div>
+              <button className="button-apply-job">Aplicar</button>
+            </article>
+          );
+        })}
+
       </div>
       <nav className="pagination">
         <a href="#">
